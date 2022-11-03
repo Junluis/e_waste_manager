1a86798618728a0041b2655dbe5954174e9562f9
SamTorreda <70458045+czrest@users.noreply.github.com>
2022-11-03 20:54:32 +0800

    UI Update | Register DatePicker
    Minor UI update - Login | Register
    Added a DatePicker onclick on date of birth

--- app/src/main/java/com/capstone/e_waste_manager/Register.java
+++ app/src/main/java/com/capstone/e_waste_manager/Register.java
@@ -2,13 +2,47 @@ package com.capstone.e_waste_manager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class Register extends AppCompatActivity {
+    EditText dateOfBirth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        //date picker start
+        dateOfBirth = findViewById(R.id.dateOfBirth);
+
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        dateOfBirth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        Register.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        month = month+1;
+                        String date = day+"/"+month+"/"+year;
+                        dateOfBirth.setText(date);
+                    }
+                }, year,month,day);
+                datePickerDialog.show();
+            }
+        });
+        //date picker end
+
     }
 }
