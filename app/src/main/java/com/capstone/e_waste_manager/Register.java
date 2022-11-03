117666e1f3eea4573175f20f82a019da09370b51
Luisito Santiago <sjunluis@gmail.com>
2022-11-04 00:05:32 +0800

    Added ID variables
    added ID variables to login and register

--- app/src/main/java/com/capstone/e_waste_manager/Register.java
+++ app/src/main/java/com/capstone/e_waste_manager/Register.java
@@ -3,12 +3,34 @@ package com.capstone.e_waste_manager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class Register extends AppCompatActivity {
+
+    TextInputEditText regUsername, regPassword, regConfPassword, regEmail, regFirstName, regLastName;
+    EditText dateOfBirth;
+    Button regRegister;
+    TextView regLogin, regPrivacyPolicy, regTermsService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        regUsername = findViewById(R.id.regUsername);
+        regPassword = findViewById(R.id.regPassword);
+        regConfPassword = findViewById(R.id.regConfPassword);
+        regEmail = findViewById(R.id.regEmail);
+        regFirstName = findViewById(R.id.regFirstName);
+        regLastName = findViewById(R.id.regLastName);
+        dateOfBirth = findViewById(R.id.dateOfBirth);
+        regRegister = findViewById(R.id.regRegister);
+        regLogin = findViewById(R.id.regLogin);
+        regPrivacyPolicy = findViewById(R.id.regPrivacyPolicy);
+        regTermsService = findViewById(R.id.regTermsService);
     }
 }
