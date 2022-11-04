3229afed0f25e1c4ec6041a9388277c0630e9676
Luisito Santiago <sjunluis@gmail.com>
2022-11-04 19:17:12 +0800

    Login/Register
    login and register

--- app/src/main/java/com/capstone/e_waste_manager/Home.java
+++ app/src/main/java/com/capstone/e_waste_manager/Home.java
@@ -1,14 +1,33 @@
 package com.capstone.e_waste_manager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class Home extends AppCompatActivity {
+
+    RecyclerView homeRecycler;
+    ImageButton homeBtnHome, homeBtnPost, homeBtnLearn, homeBtnMenu, homeBtnUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        homeRecycler = findViewById(R.id.homeRecycler);
+        homeBtnHome = findViewById(R.id.homeBtnHome);
+        homeBtnPost = findViewById(R.id.homeBtnPost);
+        homeBtnLearn = findViewById(R.id.homeBtnLearn);
+        homeBtnMenu = findViewById(R.id.homeBtnMenu);
+        homeBtnUser = findViewById(R.id.homeBtnUser);
+
+        homeBtnHome.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), Home.class)));
+
+        homeBtnPost.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), Post.class)));
+
+        homeBtnLearn.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), Learn.class)));
     }
 }
--- app/src/main/java/com/capstone/e_waste_manager/Post.java
+++ app/src/main/java/com/capstone/e_waste_manager/Post.java
@@ -2,13 +2,30 @@ package com.capstone.e_waste_manager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Post extends AppCompatActivity {
+
+    ImageButton postBtnHome, postBtnPost, postBtnLearn, postBtnMenu, postBtnUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        postBtnHome = findViewById(R.id.postBtnHome);
+        postBtnPost = findViewById(R.id.postBtnPost);
+        postBtnLearn = findViewById(R.id.postBtnLearn);
+        postBtnMenu = findViewById(R.id.postBtnMenu);
+        postBtnUser = findViewById(R.id.postBtnUser);
+
+        postBtnHome.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), Home.class)));
+
+        postBtnPost.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), Post.class)));
+
+       postBtnLearn.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), Learn.class)));
     }
 }
