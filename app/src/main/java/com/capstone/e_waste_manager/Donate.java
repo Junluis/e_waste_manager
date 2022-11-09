7fd2ee432970e00490396533563bfeb59d1a7c3f
SamTorreda <70458045+czrest@users.noreply.github.com>
2022-11-09 14:07:26 +0800

    Post UI | Profile UI | Donate UI

--- app/src/main/java/com/capstone/e_waste_manager/Donate.java
+++ app/src/main/java/com/capstone/e_waste_manager/Donate.java
@@ -3,12 +3,29 @@ package com.capstone.e_waste_manager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Donate extends AppCompatActivity {
+
+
+    ImageButton closedd;
+    Button regNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donate);
+
+        closedd = findViewById(R.id.closedd);
+        regNext = findViewById(R.id.regNext);
+
+        closedd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 }
--- app/src/main/java/com/capstone/e_waste_manager/Post.java
+++ app/src/main/java/com/capstone/e_waste_manager/Post.java
@@ -2,19 +2,31 @@ package com.capstone.e_waste_manager;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
@@ -26,9 +38,11 @@ import java.util.UUID;
 
 public class Post extends AppCompatActivity {
 
-    ImageButton postBtnHome, postBtnPost, postBtnLearn, postBtnMenu, postBtnUser;
+    ImageView morebtn, linkbtn, postbodybtn, galleryuploadbtn, uploadimgbtn;
+    ImageButton closepg;
     Button postButton;
-    EditText postTitle, postBody;
+    EditText postTitle, postBody, postlink;
+    BottomSheetBehavior bottomSheetBehavior;
 
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
@@ -38,22 +52,161 @@ public class Post extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
 
+
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-        postBtnHome = findViewById(R.id.postBtnHome);
-        postBtnPost = findViewById(R.id.postBtnPost);
-        postBtnLearn = findViewById(R.id.postBtnLearn);
-        postBtnMenu = findViewById(R.id.postBtnMenu);
-        postBtnUser = findViewById(R.id.postBtnUser);
+        closepg = findViewById(R.id.closepg);
         postButton = findViewById(R.id.postButton);
         postTitle = findViewById(R.id.postTitle);
         postBody = findViewById(R.id.postBody);
+        postlink = findViewById(R.id.postlink);
 
-        postBtnHome.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), Home.class)));
-        postBtnPost.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), Post.class)));
-        postBtnLearn.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), Learn.class)));
+        //footer buttons
+        morebtn = findViewById(R.id.morebtn);
+        linkbtn = findViewById(R.id.linkbtn);
+        postbodybtn = findViewById(R.id.postbodybtn);
+        uploadimgbtn = findViewById(R.id.uploadimgbtn);
+        galleryuploadbtn = findViewById(R.id.galleryuploadbtn);
+
+
+        //post buttons
+
+        LinearLayout bottomSheetLayout = findViewById(R.id.bottom_sheet);
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheetLayout);
+        LinearLayout uploadimgbtn2 = bottomSheetLayout.findViewById(R.id.uploadimgbtn);
+        LinearLayout postbodybtn2 = bottomSheetLayout.findViewById(R.id.postbodybtn);
+        LinearLayout linkbtn2 = bottomSheetLayout.findViewById(R.id.linkbtn);
+
+        //bottom sheet
+        morebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog();
+            }
+        });
+
+        uploadimgbtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadimgbtn.setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.green1));
+                postbodybtn.setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.darkgray));
+                linkbtn.setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.darkgray));
+                galleryuploadbtn.setVisibility(View.VISIBLE);
+                postlink.setVisibility(View.GONE);
+                postBody.requestFocus();
+            }
+        });
+        postbodybtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadimgbtn.setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.darkgray));
+                postbodybtn.setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.green1));
+                linkbtn.setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.darkgray));
+                galleryuploadbtn.setVisibility(View.GONE);
+                postlink.setVisibility(View.GONE);
+                postBody.requestFocus();
+            }
+        });
+        linkbtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadimgbtn.setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.darkgray));
+                postbodybtn.setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.darkgray));
+                linkbtn.setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.green1));
+                galleryuploadbtn.setVisibility(View.GONE);
+                postlink.setVisibility(View.VISIBLE);
+                postlink.requestFocus();
+            }
+        });
+
+        postTitle.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if(b) {
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+                }
+            }
+        });
+        postBody.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if(b) {
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+                }
+            }
+        });
+        postlink.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if(b) {
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+                }
+            }
+        });
+        postTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+            }
+        });
+        postBody.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+            }
+        });
+        postlink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+            }
+        });
+
+        closepg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        uploadimgbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadimgbtn.setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.green1));
+                postbodybtn.setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.darkgray));
+                linkbtn.setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.darkgray));
+                galleryuploadbtn.setVisibility(View.VISIBLE);
+                postlink.setVisibility(View.GONE);
+                postBody.requestFocus();
+            }
+        });
+        postbodybtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadimgbtn.setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.darkgray));
+                postbodybtn.setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.green1));
+                linkbtn.setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.darkgray));
+                galleryuploadbtn.setVisibility(View.GONE);
+                postlink.setVisibility(View.GONE);
+                postBody.requestFocus();
+            }
+        });
+        linkbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadimgbtn.setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.darkgray));
+                postbodybtn.setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.darkgray));
+                linkbtn.setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.green1));
+                galleryuploadbtn.setVisibility(View.GONE);
+                postlink.setVisibility(View.VISIBLE);
+                postlink.requestFocus();
+            }
+        });
+
+
+
         String name = user.getDisplayName();
 
         postButton.setOnClickListener(new View.OnClickListener() {
@@ -96,4 +249,30 @@ public class Post extends AppCompatActivity {
             }
         });
     }
+
+    private void showDialog(){
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+
+        LinearLayout bottomSheetLayout = findViewById(R.id.bottom_sheet);
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheetLayout);
+
+        new CountDownTimer(500, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+            }
+            public void onFinish() {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            }
+        }.start();
+
+    }
+
+
+
+
+
 }
