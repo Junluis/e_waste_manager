f73ba6d2fb53f418effff735e385e43f587dd988
Junluis <sjunluis@gmail.com>
2022-11-09 16:20:45 +0800

    Revert "Update UI"

--- app/src/main/java/com/capstone/e_waste_manager/UserProfilePage.java
+++ app/src/main/java/com/capstone/e_waste_manager/UserProfilePage.java
@@ -1,29 +1,14 @@
 package com.capstone.e_waste_manager;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
+
 import android.os.Bundle;
 
 public class UserProfilePage extends AppCompatActivity {
-    ImageButton closeup;
-    Button editProfilebtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile_page);
-
-
-        closeup = findViewById(R.id.closeup);
-        editProfilebtn = findViewById(R.id.editProfilebtn);
-
-        closeup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onBackPressed();
-            }
-        });
-
     }
 }
