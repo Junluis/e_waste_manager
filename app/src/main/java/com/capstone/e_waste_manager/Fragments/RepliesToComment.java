6b61056d5eeb24c6d36b6b1a6d65dc93f3b4fe51
SamTorreda <70458045+czrest@users.noreply.github.com>
2022-11-28 07:44:24 +0800

    test

--- app/src/main/java/com/capstone/e_waste_manager/Fragments/RepliesToComment.java
+++ app/src/main/java/com/capstone/e_waste_manager/Fragments/RepliesToComment.java
@@ -51,6 +51,7 @@ import java.io.Serializable;
 
 public class RepliesToComment extends BottomSheetDialogFragment {
 
+
     private boolean isTouch = false;
     boolean identifier = false;
     BottomSheetDialog dialog;
