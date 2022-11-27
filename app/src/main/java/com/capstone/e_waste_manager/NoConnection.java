b19a602a3088882f9a219463138788145d67e0e1
Junluis <sjunluis@gmail.com>
2022-11-27 17:50:46 +0800

    Revert "Finished forum (without partnered checkmark)"

--- app/src/main/java/com/capstone/e_waste_manager/NoConnection.java
+++ app/src/main/java/com/capstone/e_waste_manager/NoConnection.java
@@ -3,22 +3,12 @@ package com.capstone.e_waste_manager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.animation.AnimationUtils;
-import android.widget.TextView;
 
 public class NoConnection extends AppCompatActivity {
-
-    TextView textView1, textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_no_connection);
-
-        textView1 = findViewById(R.id.textView1);
-        textView2 = findViewById(R.id.textView2);
-
-        textView1.animate().alpha(1).setDuration(1000).setStartDelay(1000);
-        textView2.animate().alpha(1).setDuration(1000).setStartDelay(1500);
     }
 }
